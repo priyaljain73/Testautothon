7ea927f40d71075dfa2327ee01efd742ae7e2ca3
Sajeel <sajeelaafaq@gmail.com>
2018-08-29 13:20:33 +0530

    JSONObjects

---
+++ src/test/java/utils/JSONObject.java
@@ -0,0 +1,17 @@
+package utils;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JSONObjects {
+
+    public void createJSON()
+    {
+        JSONObject jo = new JSONObject();
+        JSONArray movies = new JSONArray();
+
+        JSONObject movie1 = new JSONObject();
+        movie1.put("movieid", "1");
+    }
+
+}
