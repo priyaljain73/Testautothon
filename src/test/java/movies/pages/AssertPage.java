4041eadb40508b8a9479fc7a67451252fe79c25e
Ravi Beck <ravibeck007@gmail.com>
2018-08-29 21:52:28 +0530

    Update: update assert
    Update: update assert

--- src/test/java/movies/pages/AssertPage.java
+++ src/test/java/movies/pages/AssertPage.java
@@ -1,22 +1,14 @@
 package movies.pages;
 
-import net.serenitybdd.core.annotations.findby.FindBy;
-import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.pages.PageObject;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class AssertPage extends PageObject {
 
-    public Boolean assertDirectorNames(String actual,String expected) {
+    public Boolean assertKeyValues(String[] actual, String[] expected) {
 
-        if(actual.equals(expected))
-            return true;
-        else
-            return false;
+        return Arrays.equals(actual, expected);
     }
 
 }
