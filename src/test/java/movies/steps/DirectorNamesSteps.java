8bcbd39f899fa740fe674782569384f217dfcee2
Viswanatha Reddy <viswanatha.reddy@gdn-commerce.com>
2018-08-29 12:56:27 +0530

    conditional case

--- src/test/java/movies/steps/DirectorNamesSteps.java
+++ src/test/java/movies/steps/DirectorNamesSteps.java
@@ -1,8 +1,14 @@
 package movies.steps;
 
+import configPackage.Config;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import movies.pages.ThreadPage;
+import movies.pages.ThreadPage2;
+import movies.stepImpl.DirectorNameStepsImpl;
+import net.serenitybdd.screenplay.actions.Switch;
+import org.springframework.beans.factory.annotation.Autowired;
 import movies.pages.GoogleSearchPage;
 import movies.pages.WikiIMDbPage;
 import utils.ThreadInfo;
@@ -16,12 +22,16 @@ import static configPackage.Config.runmode;
 public class DirectorNamesSteps {
 
     public static ThreadInfo threadInfo;
+    public String runmode=Config.runmode;
 
+    DirectorNameStepsImpl directorNameStepsimpl=new DirectorNameStepsImpl();
 
     @Given("^a list of movie name and urls$")
     public void aListOfMovieNameAndUrls() throws Exception {
 
-        System.out.println("current run mode" + runmode);
+
+
+        System.out.println("current run mode"+runmode);
         Map<String, String> movies = new HashMap<String, String>();
         movies.put("First", "first url");
         movies.put("Second", "second url");
@@ -46,17 +56,22 @@ public class DirectorNamesSteps {
     @When("^user tries to extract the director names on wikipedia and imdb$")
     public void userTriesToCompareTheDirectorNamesOnWikipediaAndImdb() throws Exception {
 
-        Class cls = WikiIMDbPage.class;
-        Object obj = cls.newInstance();
+        switch (runmode) {
+            case "api":
+                directorNameStepsimpl.checkapi();
 
-        Method m = cls.getDeclaredMethod("method1", null);
-        Method m2 = cls.getDeclaredMethod("method2", null);
-        Method m3 = cls.getDeclaredMethod("method3", null);
+                case "ui":
+                Class cls = WikiIMDbPage.class;
+                Object obj = cls.newInstance();
 
-        threadInfo.setNewMethods(obj, m, m2, m3).startThreads();
+                Method m = cls.getDeclaredMethod("method1", null);
+                Method m2 = cls.getDeclaredMethod("method2", null);
+                Method m3 = cls.getDeclaredMethod("method3", null);
 
+                threadInfo.setNewMethods(obj, m, m2, m3).startThreads();
+
+        }
     }
-
     @Then("^the director names should match$")
     public void theDirectorNamesShouldMatch() {
 
