a17ac4deaf402f4184e36c5febf2afbe9a14ee28
Viswanatha Reddy <viswanatha.reddy@gdn-commerce.com>
2018-08-29 10:37:21 +0530

    pushed the story file

---
+++ src/test/java/Movies/DirectorNamesSteps.java
@@ -0,0 +1,25 @@
+package Movies;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class DirectorNamesSteps {
+
+
+
+
+    @Given("^a list of movie name and urls$")
+    public void aListOfMovieNameAndUrls() {
+    }
+
+    @When("^user tries to compare the director names on wikipedia and imdb$")
+    public void userTriesToCompareTheDirectorNamesOnWikipediaAndImdb(){
+
+    }
+
+    @Then("^the director names should match$")
+    public void theDirectorNamesShouldMatch() {
+    }
+}
